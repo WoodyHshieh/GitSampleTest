f680228d34f4115495eafebd35c575ed1e3d5b37
Woody Hsieh <ms0075548@gmail.com>
2022-11-05 14:51:42 +0800

    Add MathBranch

--- GitSample/src/testApp/MainApp.java
+++ GitSample/src/testApp/MainApp.java
@@ -5,6 +5,16 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("2nd Version");
+		// 組長新增的 code
+		Math cMath = new Math();
+		System.out.println();
+		System.out.print(cMath.add(5, 5));
+		
 	}
 
+}
+class Math{
+	int add(int x, int y) {
+		return x+y;
+	}
 }
